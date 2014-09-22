e443f523f2a35945675b90a54560f755e0ecc526
Dharmendra <dharam3@gmail.com>
2014-09-22 17:07:52 +0530

    Efficient Calculation of number of set bit in a no.

--- src/com/dk/bitsit/BitSit.java
+++ src/com/dk/bitsit/BitSit.java
@@ -21,16 +21,44 @@ public class BitSit {
 	 */
 	public static void main(String[] args) {
 		// assert isKthBitSet(2, 1);
-		for (int i = 1; i < 10; i++)
-			System.out.println(isKthBitSet(15, i));
-
+		/*
+		 * for (int i = 1; i < 10; i++) System.out.println(isKthBitSet(15, i));
+		 */
+		testNumberOfBit();
 	}
 
-//	private static boolean isBitPalindrom(){
-//		
-//	}
+	/**
+	 * @param x
+	 *            Given number
+	 * @param k
+	 *            Bit position
+	 * @return true if kth bit is set in number x, false otherwise
+	 */
 	private static boolean isKthBitSet(int x, int k) {
 		return ((x & (1 << (k - 1))) != 0);
+	}
+
+	private static void testNumberOfBit() {
+		assert 1 == countNumberOfBits(4);
+		assert 2 == countNumberOfBits(3);
+		assert 4 == countNumberOfBits(15);
+		assert 1 == countNumberOfBits(16);
+		System.out.println("DONE");
+	}
+
+	/**
+	 * This runs only for number of 1's there in number
+	 * 
+	 * @param n
+	 * @return
+	 */
+	static int countNumberOfBits(int n) {
+		int count = 0;
+		while (n != 0) {
+			n &= n - 1;
+			count++;
+		}
+		return count;
 	}
 
 }
